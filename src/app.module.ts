b870cfc9cedcacb8f5863458acb3c5468eb1456f
Serhii_Kurilov <Serhii_Kurilov@epam.com>
2022-02-03 13:42:20 +0200

    Initital structure

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
